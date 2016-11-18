4f47a82a302001c2642c66cd92109347ce8547d9
Joshua <jrinard@hotmail.com>
2016-11-18 08:27:54 -0800

    add unit spec and backend logic

---
+++ lib/word_count.rb
@@ -0,0 +1,13 @@
+class String
+  define_method(:word_count) do |find_word|
+    word_array = []
+    counter = 0
+    sentence_array = self.split()
+    sentence_array.each() do |word|
+      if word.include?(find_word)
+        counter = counter + 1
+      end
+    end
+    counter
+  end
+end
---
+++ spec/word_count_spec.rb
@@ -0,0 +1,16 @@
+require('rspec')
+require('pry')
+require('word_count')
+
+describe('String#word_count') do
+  it ('take a word and count how many times it is in the string') do
+    expect("wolf".word_count("wolf")).to(eq(1))
+  end
+  it ('take a multiple word sentence and count a specific word once') do
+    expect("The wolf ate the rabbit".word_count("wolf")).to(eq(1))
+  end
+  it ('count how many times a specific word is used in a sentence') do
+    expect("The wolf ate the rabbit, and the wolf ran into the forest".word_count("wolf")).to(eq(2))
+  end
+
+end
